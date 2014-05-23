771e3dc141c13ca3b0964410a2e08d5c96febbd1
Pavel Shvets <shvets.pavel@gmail.com>
2014-05-23 18:58:44 +0400

    link tests

--- src/test/java/ru/parallel/octotron/core/OctoEntityTest.java
+++ src/test/java/ru/parallel/octotron/core/OctoEntityTest.java
@@ -7,11 +7,16 @@ import org.junit.rules.ExpectedException;
 import ru.parallel.octotron.generators.LinkFactory;
 import ru.parallel.octotron.generators.ObjectFactory;
 import ru.parallel.octotron.logic.TimerProcessor;
+import ru.parallel.octotron.neo4j.impl.Marker;
 import ru.parallel.octotron.neo4j.impl.Neo4jGraph;
 import ru.parallel.octotron.primitive.EDependencyType;
+import ru.parallel.octotron.primitive.EEventStatus;
 import ru.parallel.octotron.primitive.SimpleAttribute;
 import ru.parallel.octotron.primitive.exception.ExceptionModelFail;
 import ru.parallel.octotron.utils.OctoAttributeList;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class OctoEntityTest
 {
@@ -112,14 +117,22 @@ public class OctoEntityTest
 
 		Thread.sleep(1000);
 		assertEquals(0, TimerProcessor.Process().size());
+
+		entity.SetTimer("timer1", 1);
+
+		Thread.sleep(2000); // 2 secs, 1st time must timeout
+
+		OctoAttributeList list3 = TimerProcessor.Process();
+		assertEquals(1, list3.size());
+		assertEquals(entity, list3.get(0).GetParent());
 	}
 
-	private class DummySelfRule extends OctoRule
+	private class DummyRule extends OctoRule
 	{
 		private EDependencyType dep_type;
 		private long n = 0l;
 
-		DummySelfRule(String name, EDependencyType dep_type)
+		DummyRule(String name, EDependencyType dep_type)
 		{
 			super(name);
 			this.dep_type = dep_type;
@@ -158,10 +171,10 @@ public class OctoEntityTest
 		assertEquals(0, entity.Update(EDependencyType.IN));
 		assertEquals(0, entity.Update(EDependencyType.OUT));
 
-		DummySelfRule rule1 = new DummySelfRule("test2", EDependencyType.ALL);
-		DummySelfRule rule2 = new DummySelfRule("test1", EDependencyType.SELF);
-		DummySelfRule rule3 = new DummySelfRule("test3", EDependencyType.IN);
-		DummySelfRule rule4 = new DummySelfRule("test4", EDependencyType.OUT);
+		DummyRule rule1 = new DummyRule("test2", EDependencyType.ALL);
+		DummyRule rule2 = new DummyRule("test1", EDependencyType.SELF);
+		DummyRule rule3 = new DummyRule("test3", EDependencyType.IN);
+		DummyRule rule4 = new DummyRule("test4", EDependencyType.OUT);
 
 		entity.AddRule(rule1);
 		entity.AddRule(rule2);
@@ -176,5 +189,243 @@ public class OctoEntityTest
 		assertEquals(1, rule2.GetN());
 		assertEquals(1, rule3.GetN());
 		assertEquals(1, rule4.GetN());
+	}
+
+	@Test
+	public void TestAddReaction()
+	{
+		OctoEntity entity = OctoEntityTest.obj_factory.Create();
+
+		entity.DeclareAttribute("test", 0);
+		OctoReaction reaction = new OctoReaction("test", 1
+			, new OctoResponse(EEventStatus.INFO, "test"));
+
+		entity.AddReaction(reaction);
+	}
+
+	@Test
+	public void TestAddReactions()
+	{
+		OctoEntity entity = OctoEntityTest.obj_factory.Create();
+
+		entity.DeclareAttribute("test", 0);
+
+		List<OctoReaction> reactions = new LinkedList<OctoReaction>();
+
+		reactions.add(new OctoReaction("test", 1
+			, new OctoResponse(EEventStatus.INFO, "test")));
+		reactions.add(new OctoReaction("test", 2
+			, new OctoResponse(EEventStatus.INFO, "test")));
+
+		entity.AddReactions(reactions);
+	}
+
+	@Test
+	public void TestAddRule()
+	{
+		OctoEntity entity = OctoEntityTest.obj_factory.Create();
+
+		entity.AddRule(new DummyRule("test1", EDependencyType.ALL));
+	}
+
+	@Test
+	public void TestAddRules()
+	{
+		OctoEntity entity = OctoEntityTest.obj_factory.Create();
+
+		entity.DeclareAttribute("test", 0);
+
+		List<OctoRule> rules = new LinkedList<OctoRule>();
+
+		rules.add(new DummyRule("test1", EDependencyType.ALL));
+		rules.add(new DummyRule("test2", EDependencyType.ALL));
+
+		entity.AddRules(rules);
+	}
+
+	@Test
+	public void TestAddMarker()
+	{
+		OctoEntity entity = OctoEntityTest.obj_factory.Create();
+
+		entity.DeclareAttribute("test", 0);
+		OctoReaction reaction = new OctoReaction("test", 1
+			, new OctoResponse(EEventStatus.INFO, "test"));
+
+		entity.AddReaction(reaction);
+
+		entity.AddMarker(reaction.GetID(), "test1", true);
+		entity.AddMarker(reaction.GetID(), "test2", false);
+	}
+
+	@Test
+	public void TestDeleteMarker()
+	{
+		OctoEntity entity = OctoEntityTest.obj_factory.Create();
+
+		entity.DeclareAttribute("test", 0);
+		OctoReaction reaction = new OctoReaction("test", 1
+			, new OctoResponse(EEventStatus.INFO, "test"));
+
+		entity.AddReaction(reaction);
+
+		assertEquals(0, entity.GetMarkers().size());
+
+		long id1 = entity.AddMarker(reaction.GetID(), "test1", true);
+		long id2 = entity.AddMarker(reaction.GetID(), "test2", false);
+
+		assertEquals(2, entity.GetMarkers().size());
+
+		entity.DeleteMarker(id1);
+		assertEquals(1, entity.GetMarkers().size());
+
+		entity.DeleteMarker(id2);
+		assertEquals(0, entity.GetMarkers().size());
+	}
+
+
+	@Test
+	public void TestGetRule()
+	{
+		OctoEntity entity = OctoEntityTest.obj_factory.Create();
+
+		final int N = 10;
+
+		for(int i = 0; i < N; i++)
+		{
+			entity.AddRule(new DummyRule("test" + i, EDependencyType.ALL));
+
+			List<OctoRule> rules = entity.GetRules();
+
+			assertEquals(i + 1, rules.size());
+
+			for(int j = 0; j < i + 1; j++)
+				assertEquals("test" + j, rules.get(j).GetAttr());
+		}
+	}
+
+	@Test
+	public void TestGetReactions()
+	{
+		OctoEntity entity = OctoEntityTest.obj_factory.Create();
+		entity.DeclareAttribute("test", 0);
+
+		final int N = 10;
+
+		for(int i = 0; i < N; i++)
+		{
+			OctoReaction reaction = new OctoReaction("test" + i, 1
+				, new OctoResponse(EEventStatus.INFO, "test"));
+
+			entity.AddReaction(reaction);
+
+			List<OctoReaction> reactions = entity.GetReactions();
+
+			assertEquals(i + 1, reactions.size());
+
+			for(int j = 0; j < i + 1; j++)
+				assertEquals("test" + j, reactions.get(j).GetCheckName());
+		}
+	}
+
+
+	@Test
+	public void TestGetMarkers()
+	{
+		OctoEntity entity = OctoEntityTest.obj_factory.Create();
+
+		entity.DeclareAttribute("test", 0);
+		OctoReaction reaction = new OctoReaction("test", 1
+			, new OctoResponse(EEventStatus.INFO, "test"));
+
+		entity.AddReaction(reaction);
+
+		final int N = 10;
+
+		for(int i = 0; i < N; i++)
+		{
+			entity.AddMarker(reaction.GetID(), "test" + i, true);
+
+			List<Marker> markers = entity.GetMarkers();
+
+			assertEquals(i + 1, markers.size());
+
+			for(int j = 0; j < i + 1; j++)
+				assertEquals("test" + j, markers.get(j).GetDescription());
+		}
+	}
+
+	@Test
+	public void TestEquals()
+	{
+		OctoEntity entity1 = OctoEntityTest.obj_factory.Create();
+		OctoEntity entity2 = OctoEntityTest.obj_factory.Create();
+
+		OctoEntity entity3 = OctoEntityTest.link_factory.OneToOne(OctoEntityTest.obj_factory.Create(), OctoEntityTest.obj_factory.Create());
+		OctoEntity entity4 = OctoEntityTest.link_factory.OneToOne(OctoEntityTest.obj_factory.Create(), OctoEntityTest.obj_factory.Create());
+
+		assertTrue (entity1.equals(entity1));
+		assertFalse(entity1.equals(entity2));
+		assertFalse(entity1.equals(entity3));
+		assertFalse(entity1.equals(entity4));
+
+		assertFalse(entity2.equals(entity1));
+		assertTrue(entity2.equals(entity2));
+		assertFalse(entity2.equals(entity3));
+		assertFalse(entity2.equals(entity4));
+
+		assertFalse(entity3.equals(entity1));
+		assertFalse(entity3.equals(entity2));
+		assertTrue(entity3.equals(entity3));
+		assertFalse(entity3.equals(entity4));
+
+		assertFalse(entity4.equals(entity1));
+		assertFalse(entity4.equals(entity2));
+		assertFalse(entity4.equals(entity3));
+		assertTrue(entity4.equals(entity4));
+	}
+
+	@Test
+	public void TestSetReactionState()
+	{
+		OctoEntity entity = OctoEntityTest.obj_factory.Create();
+
+		entity.DeclareAttribute("test", 0);
+
+		OctoReaction reaction = new OctoReaction("test", 1
+			, new OctoResponse(EEventStatus.INFO, "test"));
+
+		entity.AddReaction(reaction);
+
+		entity.SetReactionState(reaction.GetID(), 0);
+		entity.SetReactionState(reaction.GetID(), 1);
+	}
+
+	@Test
+	public void TestGetReactionState()
+	{
+		final int N = 4;
+
+		OctoEntity entity = OctoEntityTest.obj_factory.Create();
+
+		entity.DeclareAttribute("test", 0);
+
+		OctoReaction reaction1 = new OctoReaction("test", 1
+			, new OctoResponse(EEventStatus.INFO, "test"));
+
+		OctoReaction reaction2 = new OctoReaction("test", 1
+			, new OctoResponse(EEventStatus.INFO, "test"));
+
+		entity.AddReaction(reaction1);
+		entity.AddReaction(reaction2);
+
+		for(int i = 0; i < N; i++)
+		{
+			entity.SetReactionState(reaction1.GetID(), i);
+			entity.SetReactionState(reaction2.GetID(), N-i);
+
+			assertEquals(i, entity.GetReactionState(reaction1.GetID()));
+			assertEquals(N-i, entity.GetReactionState(reaction2.GetID()));
+		}
 	}
 }
--- src/test/java/ru/parallel/octotron/core/OctoLinkTest.java
+++ src/test/java/ru/parallel/octotron/core/OctoLinkTest.java
@@ -1,8 +1,71 @@
 package ru.parallel.octotron.core;
 
+import org.junit.*;
+import ru.parallel.octotron.generators.LinkFactory;
+import ru.parallel.octotron.generators.ObjectFactory;
+import ru.parallel.octotron.neo4j.impl.Neo4jGraph;
+import ru.parallel.octotron.primitive.SimpleAttribute;
+
 import static org.junit.Assert.*;
 
 public class OctoLinkTest
 {
+	private static Neo4jGraph graph;
+	private static GraphService graph_service;
 
+	private static ObjectFactory obj_factory;
+	private static LinkFactory link_factory;
+
+	@BeforeClass
+	public static void Init() throws Exception
+	{
+		OctoLinkTest.graph = new Neo4jGraph("dbs/test_node", Neo4jGraph.Op.RECREATE);
+		OctoLinkTest.graph_service = new GraphService(OctoLinkTest.graph);
+
+		SimpleAttribute[] obj_att = {
+			new SimpleAttribute("object", "ok")
+		};
+
+		OctoLinkTest.obj_factory = new ObjectFactory(OctoLinkTest.graph_service).Attributes(obj_att);
+
+		SimpleAttribute[] link_att = {
+			new SimpleAttribute("link", "ok"),
+			new SimpleAttribute("type", "contain"),
+		};
+
+		OctoLinkTest.link_factory = new LinkFactory(OctoLinkTest.graph_service).Attributes(link_att);
+	}
+
+	@AfterClass
+	public static void Delete() throws Exception
+	{
+		OctoLinkTest.graph.Shutdown();
+		OctoLinkTest.graph.Delete();
+	}
+
+	@Test
+	public void TestSource()
+	{
+		OctoObject object1 = obj_factory.Create();
+		OctoObject object2 = obj_factory.Create();
+
+		OctoLink link1 = link_factory.OneToOne(object1, object2);
+		OctoLink link2 = link_factory.OneToOne(object2, object1);
+
+		assertEquals(object1, link1.Source());
+		assertEquals(object2, link2.Source());
+	}
+
+	@Test
+	public void TestTarget()
+	{
+		OctoObject object1 = obj_factory.Create();
+		OctoObject object2 = obj_factory.Create();
+
+		OctoLink link1 = link_factory.OneToOne(object1, object2);
+		OctoLink link2 = link_factory.OneToOne(object2, object1);
+
+		assertEquals(object2, link1.Target());
+		assertEquals(object1, link2.Target());
+	}
 }

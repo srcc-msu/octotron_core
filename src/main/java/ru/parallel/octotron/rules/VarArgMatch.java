bed5a8369c5be8ab10a4d0179c2a70c52fe7849b
Pavel Shvets <shvets.pavel@gmail.com>
2014-06-06 20:22:31 +0400

    new rules for comparing sensor values

---
+++ src/main/java/ru/parallel/octotron/rules/VarArgMatch.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2014 SRCC MSU
+ * 
+ * Distributed under the MIT License - see the accompanying file LICENSE.txt.
+ ******************************************************************************/
+
+package ru.parallel.octotron.rules;
+
+import ru.parallel.octotron.core.OctoAttribute;
+import ru.parallel.octotron.core.OctoEntity;
+import ru.parallel.octotron.core.OctoRule;
+import ru.parallel.octotron.primitive.EDependencyType;
+
+public class VarArgMatch extends OctoRule
+{
+	private static final long serialVersionUID = -665317574895287470L;
+	private final String param;
+	private final String match_arg;
+
+	public VarArgMatch(String attribute_name, String param, String match_arg)
+	{
+		super(attribute_name);
+		this.param = param;
+		this.match_arg = match_arg;
+	}
+
+	@Override
+	public EDependencyType GetDeps()
+	{
+		return EDependencyType.SELF;
+	}
+
+	@Override
+	public Object Compute(OctoEntity entity)
+	{
+		OctoAttribute attr = entity.GetAttribute(param);
+		OctoAttribute match_attr = entity.GetAttribute(match_arg);
+
+		if(attr.GetCTime() == 0 || !attr.IsValid())
+			return GetDefaultValue();
+
+		if(match_attr.GetCTime() == 0 || !match_attr.IsValid())
+			return GetDefaultValue();
+
+		return attr.eq(match_attr.GetValue());
+	}
+
+	@Override
+	public Object GetDefaultValue()
+	{
+		return true;
+	}
+}
---
+++ src/main/java/ru/parallel/octotron/rules/VarArgMatchAprx.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2014 SRCC MSU
+ * 
+ * Distributed under the MIT License - see the accompanying file LICENSE.txt.
+ ******************************************************************************/
+
+package ru.parallel.octotron.rules;
+
+import ru.parallel.octotron.core.OctoAttribute;
+import ru.parallel.octotron.core.OctoEntity;
+import ru.parallel.octotron.core.OctoRule;
+import ru.parallel.octotron.primitive.EDependencyType;
+
+public class VarArgMatchAprx extends OctoRule
+{
+	private static final long serialVersionUID = -665317574895287470L;
+	private final String param;
+	private final String match_arg;
+	private final Object aprx;
+
+	public VarArgMatchAprx(String attribute_name, String param, String match_arg, Object aprx)
+	{
+		super(attribute_name);
+		this.param = param;
+		this.match_arg = match_arg;
+		this.aprx = aprx;
+	}
+
+	@Override
+	public EDependencyType GetDeps()
+	{
+		return EDependencyType.SELF;
+	}
+
+	@Override
+	public Object Compute(OctoEntity entity)
+	{
+		OctoAttribute attr = entity.GetAttribute(param);
+		OctoAttribute match_attr = entity.GetAttribute(match_arg);
+
+		if(attr.GetCTime() == 0 || !attr.IsValid())
+			return GetDefaultValue();
+
+		if(match_attr.GetCTime() == 0 || !match_attr.IsValid())
+			return GetDefaultValue();
+
+		return attr.aeq(match_attr.GetValue(), aprx);
+	}
+
+	@Override
+	public Object GetDefaultValue()
+	{
+		return true;
+	}
+}

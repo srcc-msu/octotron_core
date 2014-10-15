6e5840a3ded79821e5eb59c75993ab5ad5d16c24
Pavel Shvets <shvets.pavel@gmail.com>
2014-10-15 15:08:16 +0400

    added license info

--- src/main/java/ru/parallel/octotron/core/logic/Reaction.java
+++ src/main/java/ru/parallel/octotron/core/logic/Reaction.java
@@ -1,3 +1,9 @@
+/*******************************************************************************
+ * Copyright (c) 2014 SRCC MSU
+ *
+ * Distributed under the MIT License - see the accompanying file LICENSE.txt.
+ ******************************************************************************/
+
 package ru.parallel.octotron.core.logic;
 
 import ru.parallel.octotron.core.model.IModelAttribute;
